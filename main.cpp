6f6d727496ebc3c835ab5e23a44e63ac1bbf1aa2
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-03-30 02:59:02 +0200

    Creating a window with GLFW

--- main.cpp
+++ main.cpp
@@ -1,29 +1,52 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+
 class HelloTriangleApplication {
+private:
+	GLFWwindow* window;
+
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
 private:
+	void initWindow() {
+		glfwInit();
+
+		/* Tell GLFW not to produce OpenGL context */
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
 	void initVulkan() {
 
 	}
 
 	void mainLoop() {
-		
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup() {
+		glfwDestroyWindow(window);
 
+		glfwTerminate();
 	}
 };
 
